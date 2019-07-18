aa84a124300495409ae8e184a22bd7821773f1ef
citra <citraswramli93@gmail.com>
2019-07-18 18:36:04 +0700

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ const UserController = require('../controllers/user');
 
 const router = express.Router();
 
+//buat user
 router.post('/register', UserController.postRegister);
 
 router.post('/login', UserController.postLogin);
